3868ecf885e5cdd17bb832cc59486e834949f13f
Ecaree <2551728057@qq.com>
2026-01-28 23:28:07 +0800

    ✨ RemapJarTask 支持独立配置

--- src/main/java/com/ecaree/jarremapper/task/RemapJarTask.java
+++ src/main/java/com/ecaree/jarremapper/task/RemapJarTask.java
@@ -7,6 +7,9 @@ import com.ecaree.jarremapper.remap.JarRemapper;
 import lombok.Getter;
 import lombok.Setter;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.Optional;
@@ -17,32 +20,78 @@ import java.io.File;
 import java.io.IOException;
 
 public class RemapJarTask extends DefaultTask {
+    /**
+     * 独立模式：直接指定输入 JAR
+     * 优先级高于 extension 配置
+     */
+    @InputFile
+    @Optional
+    @Getter
+    private final RegularFileProperty inputJar = getProject().getObjects().fileProperty();
+
+    /**
+     * 独立模式：直接指定输出 JAR
+     * 优先级高于 extension 配置
+     */
+    @OutputFile
+    @Optional
+    @Getter
+    private final RegularFileProperty outputJar = getProject().getObjects().fileProperty();
+
+    /**
+     * 独立模式：直接指定映射文件
+     * 优先级高于 extension 配置
+     */
+    @InputFile
+    @Optional
+    @Getter
+    private final RegularFileProperty mappingFile = getProject().getObjects().fileProperty();
+
+    /**
+     * 是否反向映射
+     * 用于将可读命名转回混淆命名
+     */
+    @Input
+    @Optional
+    @Getter
+    private final Property<Boolean> reverse = getProject().getObjects().property(Boolean.class);
+
     @Internal
     @Getter
     @Setter
     private JarRemapperExtension extension;
 
     @InputFile
-    public File getInputJar() {
+    public File getEffectiveInputJar() {
+        if (inputJar.isPresent()) {
+            return inputJar.get().getAsFile();
+        }
         return extension.getInputJar().get().getAsFile();
+    }
+
+    @OutputFile
+    public File getEffectiveOutputJar() {
+        if (outputJar.isPresent()) {
+            return outputJar.get().getAsFile();
+        }
+        return extension.getOutputJar().get().getAsFile();
     }
 
     @InputFile
     @Optional
-    public File getMappingFile() {
-        return extension.getEffectiveMappingFile();
-    }
-
-    @OutputFile
-    public File getOutputJar() {
-        return extension.getOutputJar().get().getAsFile();
+    public File getEffectiveMappingFile() {
+        if (mappingFile.isPresent()) {
+            return mappingFile.get().getAsFile();
+        }
+        return extension != null ? extension.getEffectiveMappingFile() : null;
     }
 
     @TaskAction
     public void remapJar() throws IOException {
-        File inputJar = getInputJar();
-        File outputJar = getOutputJar();
-        File mappingFile = getMappingFile();
+        File inputJar = getEffectiveInputJar();
+        File outputJar = getEffectiveOutputJar();
+        File mappingFile = getEffectiveMappingFile();
+        boolean reverse = this.reverse.getOrElse(false);
 
         if (!inputJar.exists()) {
             throw new RuntimeException("Input JAR file does not exist: " + inputJar);
@@ -56,8 +105,9 @@ public class RemapJarTask extends DefaultTask {
         getLogger().lifecycle("Input: {}", inputJar);
         getLogger().lifecycle("Output: {}", outputJar);
         getLogger().lifecycle("Mapping: {}", mappingFile);
+        getLogger().lifecycle("Reverse: {}", reverse);
 
-        MappingData mappingData = MappingLoader.load(mappingFile);
+        MappingData mappingData = MappingLoader.load(mappingFile, reverse);
         getLogger().lifecycle("Loaded mappings: {} classes, {} fields, {} methods",
                 mappingData.getClassCount(),
                 mappingData.getFieldCount(),
